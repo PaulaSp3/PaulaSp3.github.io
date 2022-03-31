09123a11563a4827de31187182d786986a202deb
lwdtirol <lwd@tirol.gv.at>
2022-03-31 17:24:55 +0200

    Leaflet code in extra File main.js geschrieben

---
+++ nz/main.js
@@ -0,0 +1,14 @@
+/*Neuseelandreise Skript */
+// einzeilger Kommentar
+
+/*Link von Leaflet Startseite*/
+        /*L: Klasse Leaflet (map: methode)*/
+        var map = L.map('map'/*Referenz zu div id=map*/).setView([-39.49, 176.90], 10);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { /*Dictionary*/
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([-39.49, 176.90]).addTo(map)
+            .bindPopup('<h3>Napier</h3>')
+            .openPopup();
