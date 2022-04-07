a9797c20abee460c7e4861eaf6cfe12a7bbb2137
lwdtirol <lwd@tirol.gv.at>
2022-04-07 15:52:37 +0200

    Koordinaten als Array geschrieben

--- nz/main.js
+++ nz/main.js
@@ -6,13 +6,16 @@
 let lat = -39.49
 let lng = 176.90
 let zoom = 10
-let map = L.map('map' /*Referenz zu div id=map*/ ).setView([lat, lng], zoom);
+
+// array: []
+let coords = [-39.49, 176.90]
+let map = L.map('map' /*Referenz zu div id=map*/ ).setView(coords, zoom);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     /*Dictionary*/
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([lat, lng]).addTo(map)
+L.marker(coords).addTo(map)
     .bindPopup('<h3>Napier</h3>')
     .openPopup();
