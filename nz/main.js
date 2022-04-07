3640af1cfb55c76d4cc8ef95a5f4972f281157b0
lwdtirol <lwd@tirol.gv.at>
2022-04-07 15:44:37 +0200

    lat und lon als variablen

--- nz/main.js
+++ nz/main.js
@@ -3,13 +3,16 @@
 
 /*Link von Leaflet Startseite*/
 /*L: Klasse Leaflet (map: methode)*/
-var map = L.map('map' /*Referenz zu div id=map*/ ).setView([-39.49, 176.90], 10);
+let lat = -39.49
+let lng = 176.90
+let zoom = 10
+let map = L.map('map' /*Referenz zu div id=map*/ ).setView([lat, lng], zoom);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     /*Dictionary*/
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-39.49, 176.90]).addTo(map)
+L.marker([lat, lng]).addTo(map)
     .bindPopup('<h3>Napier</h3>')
     .openPopup();
